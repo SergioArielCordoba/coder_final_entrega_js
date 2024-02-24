19a58c3392eabfae7601c7287499b1bb8135807a
SergioCordoba <sergioarielcordoba@yahoo.com.ar>
2024-02-24 00:41:21 -0300

    Se agrega funcionalidad cambio de moneda

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 
 let contenedorPrincipal = document.getElementById("contenedorPrincipal")
 let btnCart = document.getElementById("btnCart")
-
 let carrito
+
 
 function getCarritoFromLocalStorage() {
     let carritoLocalStorage = localStorage.getItem("carrito")
-
-    if (carritoLocalStorage != null) {
+    if (carritoLocalStorage) {
         let carritoLocalStorageParse = JSON.parse(carritoLocalStorage)
         carrito = carritoLocalStorageParse
     } else {
@@ -21,31 +20,65 @@ function setCarritoToLocalStorage() {
     localStorage.setItem("carrito", prodcutoJson)
 }
 
-misProductos.forEach((producto) => {
-    let contenedorDeProducto = document.createElement("div")
-    contenedorDeProducto.className = "contenedor-item"
-    contenedorDeProducto.innerHTML = `
-        <h2 class="contenedor-nombre">${producto.nombre}</h2>
-        <img src="${producto.img}" alt="">
-        <p class="contenedor-precio">${producto.precio} $ARS</p>
-    `
-    contenedorPrincipal.appendChild(contenedorDeProducto)
-    let btnAddCart = document.createElement("button")
-    btnAddCart.className = "btnAddCart"
-    btnAddCart.innerText = "Agregar al Carrito"
-    contenedorDeProducto.appendChild(btnAddCart)
-    btnAddCart.addEventListener("click", () => {
-        getCarritoFromLocalStorage()
-        let indice = carrito.findIndex((productoBuscado) => producto.id == productoBuscado.id)
-        if (indice > -1) {
-            carrito[indice].cant++
-        } else {
-            producto.cant++
-            carrito.push(producto)
-        }
-        setCarritoToLocalStorage()
+function cargarProductosAlIndex(arrayDeProductos) {
+
+    arrayDeProductos.forEach((producto) => {
+        let contenedorDeProducto = document.createElement("div")
+        contenedorDeProducto.className = "contenedor-item"
+        contenedorDeProducto.innerHTML = `
+            <h2 class="contenedor-nombre">${producto.nombre}</h2>
+            <img src="${producto.img}" alt="">
+            <p class="contenedor-precio">${producto.precio} ${producto.moneda}</p>
+        `
+        contenedorPrincipal.appendChild(contenedorDeProducto)
+        let btnAddCart = document.createElement("button")
+        btnAddCart.className = "btnAddCart"
+        btnAddCart.innerText = "Agregar al Carrito"
+        contenedorDeProducto.appendChild(btnAddCart)
+        btnAddCart.addEventListener("click", () => {
+            getCarritoFromLocalStorage()
+            let indice = carrito.findIndex((productoBuscado) => producto.id == productoBuscado.id)
+            if (indice > -1) {
+                carrito[indice].cant++
+                
+            } else {
+                
+                carrito.push(producto)
+                
+            }
+            setCarritoToLocalStorage()
+        })
     })
-})
+
+}
+
+cargarProductosAlIndex(misProductos)
+
+// misProductos.forEach((producto) => {
+//     let contenedorDeProducto = document.createElement("div")
+//     contenedorDeProducto.className = "contenedor-item"
+//     contenedorDeProducto.innerHTML = `
+//         <h2 class="contenedor-nombre">${producto.nombre}</h2>
+//         <img src="${producto.img}" alt="">
+//         <p class="contenedor-precio">${producto.precio} ${producto.moneda}</p>
+//     `
+//     contenedorPrincipal.appendChild(contenedorDeProducto)
+//     let btnAddCart = document.createElement("button")
+//     btnAddCart.className = "btnAddCart"
+//     btnAddCart.innerText = "Agregar al Carrito"
+//     contenedorDeProducto.appendChild(btnAddCart)
+//     btnAddCart.addEventListener("click", () => {
+//         getCarritoFromLocalStorage()
+//         let indice = carrito.findIndex((productoBuscado) => producto.id == productoBuscado.id)
+//         if (indice > -1) {
+//             carrito[indice].cant++
+//         } else {
+//             producto.cant++
+//             carrito.push(producto)
+//         }
+//         setCarritoToLocalStorage()
+//     })
+// })
 
 
 //Modal config -> ponerlo en otro archivo JS
@@ -54,16 +87,15 @@ let modal = document.getElementById("modalConteiner");
 let close = document.getElementById("close");
 let modalProductos = document.getElementById("modalProductos")
 
-
 activarModal();
-
 
 close.addEventListener("click", () => {
     modal.classList.remove("show")
-    let cleanModal = document.getElementById("items-carrito")
+    //let cleanModal = document.getElementById("items-carrito")
     while (modalProductos.firstChild) {
         modalProductos.removeChild(modalProductos.firstChild)
     }
+    // window.location.reload()
 })
 
 /*
@@ -76,12 +108,11 @@ function activarModal(){
     })
 }
 */
-function activarModal(){
-    btnCart.addEventListener("click",mostrarProductosDelCarrito)
-    
+function activarModal() {
+    btnCart.addEventListener("click", mostrarProductosDelCarrito)
 }
 
-function mostrarProductosDelCarrito( ){
+function mostrarProductosDelCarrito() {
     getCarritoFromLocalStorage()
     carrito.forEach(item => {
         let itemCarrito = document.createElement("div")
@@ -90,14 +121,14 @@ function mostrarProductosDelCarrito( ){
             <h3>${item.nombre}</h3>
             <p>${item.cant}</p>
             <img src="${item.img}" alt="">
-            <p>$ ${item.precio}</p>
+            <p>${item.moneda} ${item.precio}</p>
             <i class="bi bi-trash-fill btnEliminar" id ="${item.id}"></i>
         `
-        modalProductos.append(itemCarrito) 
+        modalProductos.append(itemCarrito)
     })
     modal.classList.add("show")
     eliminarProductoDelCarrito();
-    
+
 }
 
 /* 
@@ -115,19 +146,51 @@ function mostrarProductosDelCarrito(item){
 }
 */
 
-function eliminarProductoDelCarrito(){
-    
+function eliminarProductoDelCarrito() {
+
     let btnEliminar = document.querySelectorAll(".btnEliminar")
-    btnEliminar.forEach((item)=>{
-        item.addEventListener("click",(e)=>{
+    btnEliminar.forEach((item) => {
+        item.addEventListener("click", (e) => {
             let id = parseInt(e.currentTarget.id)
-            let indice = carrito.findIndex( (x)=>x.id == id)
-            carrito.splice(indice,1)
+            let indice = carrito.findIndex((x) => x.id == id)
+            carrito.splice(indice, 1)
             let borrarProductosDelModal = document.querySelectorAll(".items-carrito")
-            borrarProductosDelModal.forEach(x=>x.remove())
+            borrarProductosDelModal.forEach(x => x.remove())
+            console.log(carrito)
             setCarritoToLocalStorage()
             mostrarProductosDelCarrito()
         })
     })
 }
 
+/* USO DE DOLAR API */
+let dolarApi = "https://dolarapi.com/v1/dolares/tarjeta"
+let misProductosDolar
+
+const perdirDatos = async () => {
+    const resp = await fetch(dolarApi)
+    let data = await resp.json()
+    let misProductosDolar = misProductos.map(function (prod) {
+        prod.precio = (prod.precio / data.venta).toFixed(2)
+        return prod
+    })
+
+    misProductosDolar.forEach(producto => producto.moneda = "US$")
+    let borrarProductosDelindex = document.querySelectorAll(".contenedor-item")
+    borrarProductosDelindex.forEach(x => x.remove("contenedor-item"))
+    cargarProductosAlIndex(misProductosDolar)
+    flag=1
+}
+let flag
+
+let btnUSA = document.querySelector(".usa")
+btnUSA.addEventListener("click", () => {
+    flag ? alert("ya esta en dolar") : perdirDatos();
+})
+
+let btnArg = document.querySelector(".arg")
+btnArg.addEventListener("click", ()=>{
+    let borrarProductosDelindex = document.querySelectorAll(".contenedor-item")
+    borrarProductosDelindex.forEach(x => x.remove("contenedor-item"))
+    console.log(misProductos)
+})
--- productos.js
+++ productos.js
@@ -4,81 +4,90 @@ let misProductos = [
         nombre: "Vans Cuadricula 01",
         precio: 98000,
         img: "media/img-item/z1.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
-        
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     },
     {
         id: 2,
         nombre: "Vans Blancas y Negras MDL 05",
         precio: 110000,
         img: "media/img-item/z2.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     },
     {
         id: 3,
         nombre: "Adidas Naranjas Running",
         precio: 135000,
         img: "media/img-item/z3.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     },
     {
         id: 4,
         nombre: "Urbanas Negras",
         precio: 88000,
         img: "media/img-item/z4.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     },
     {
         id: 5,
         nombre: "Kappa Urbanas Negras",
         precio: 79000,
         img: "media/img-item/z5.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     },
     {
         id: 6,
         nombre: "Fila Elit",
         precio: 240000,
         img: "media/img-item/z6.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     },
     {
         id: 7,
         nombre: "Negras Urbanas Atomik",
         precio: 123000,
         img: "media/img-item/z7.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     },
     {
         id: 8,
         nombre: "Nike Air Max",
         precio: 95000,
         img: "media/img-item/z8.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     },
     {
         id: 9,
         nombre: "New Balance Unisex",
         precio: 170000,
         img: "media/img-item/z9.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     },
     {
         id: 10,
         nombre: "47 Street Washed",
         precio: 168000,
         img: "media/img-item/z10.webp",
-        cant: 0,
-        talle: [39,40,41,42,43,44]
+        cant: 1,
+        talle: [39,40,41,42,43,44],
+        moneda: "$ARS"
     }
 
 ]
